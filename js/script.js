714e10527a9a062dd1fb8482f2be6424fe9ac9c2
Saqib Hussnain <saqibmehar847@gmail.com>
2026-06-25 09:13:03 +0500

    added image data structure to script.js for gallery functionality

---
+++ js/script.js
@@ -0,0 +1,87 @@
+let images = [
+    {
+        id: 1,
+        title: "Mountain Sunrise",
+        category: "Nature",
+        src: "https://plus.unsplash.com/premium_photo-1676320526001-07b75bd19ae3?w=800&q=80",
+        alt: "Mountain Sunrise"
+    },
+    {
+        id: 2,
+        title: "Forest Path",
+        category: "Nature",
+        src: "https://plus.unsplash.com/premium_photo-1623967680551-3e4694e2c9ad?w=800&q=80",
+        alt: "Forest Path"
+    },
+    {
+        id: 3,
+        title: "City Skyline",
+        category: "Architecture",
+        src: "https://plus.unsplash.com/premium_photo-1541423408854-5df732b6f6d1?w=800&q=80",
+        alt: "City Skyline"
+    },
+    {
+        id: 4,
+        title: "Glass Tower",
+        category: "Architecture",
+        src: "https://plus.unsplash.com/premium_photo-1675754965318-5d31291657d4?w=800&q=80",
+        alt: "Glass Tower"
+    },
+    {
+        id: 5,
+        title: "Santorini",
+        category: "Travel",
+        src: "https://plus.unsplash.com/premium_photo-1697729900945-598459160f7b?w=800&q=80",
+        alt: "Santorini"
+    },
+    {
+        id: 6,
+        title: "Tokyo Streets",
+        category: "Travel",
+        src: "https://plus.unsplash.com/premium_photo-1573455494057-12684d151bf4?w=800&q=80",
+        alt: "Tokyo Streets"
+    },
+    {
+        id: 7,
+        title: "Color Burst",
+        category: "Abstract",
+        src: "https://plus.unsplash.com/premium_photo-1567095761054-7a02e69e5c43?w=800&q=80",
+        alt: "Color Burst"
+    },
+    {
+        id: 8,
+        title: "Neon Waves",
+        category: "Abstract",
+        src: "https://plus.unsplash.com/premium_photo-1635614017406-7c192d832072?w=800&q=80",
+        alt: "Neon Waves"
+    },
+    {
+        id: 9,
+        title: "Autumn Lake",
+        category: "Nature",
+        src: "https://plus.unsplash.com/premium_photo-1470748085385-5fbb3018c796?w=800&q=80",
+        alt: "Autumn Lake"
+    },
+    {
+        id: 10,
+        title: "Desert Dunes",
+        category: "Travel",
+        src: "https://plus.unsplash.com/premium_photo-1542401886-65d6c61db217?w=800&q=80",
+        alt: "Desert Dunes"
+    },
+    {
+        id: 11,
+        title: "Old Bridge",
+        category: "Architecture",
+        src: "https://plus.unsplash.com/premium_photo-1694084854989-24693f767f98?w=800&q=80",
+        alt: "Old Bridge"
+    },
+    {
+        id: 12,
+        title: "Fluid Art",
+        category: "Abstract",
+        src: "https://plus.unsplash.com/premium_photo-1604871000636-074fa5117945?w=800&q=80",
+        alt: "Fluid Art"
+    }
+];
+
